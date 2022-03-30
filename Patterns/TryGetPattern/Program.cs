0d2c9e8fc5dbb7f2d4cb2f4b7e912112dee37d80
ipavicic <ivanp@dump.hr>
2022-03-30 21:16:28 +0200

    Patterns project

---
+++ Patterns/TryGetPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿List<string> list = new() { "test", "test2", "test3" };
+
+bool GetValueByPattern(string search, out string item)
+{
+    item = list.FirstOrDefault(x => x == search);
+
+    return item != null;
+}
+
+string GetValueWithoutPattern(string search)
+{
+    return list.FirstOrDefault(x => x == search);
+}
+
+if (GetValueByPattern("test", out string item))
+{
+    Console.WriteLine(item);
+}
+
+var found = GetValueWithoutPattern("test4");
+if (found != null)
+{
+    Console.WriteLine(found);
+}
+
+
